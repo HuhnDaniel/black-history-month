8807fc454c670b0b41ac5bba14c3e97983f8f343
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2023-02-02 21:00:45 -0800

    add images to file tree, populate artist json file

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 let body = document.body;
-import data from './artists.json' assert { type: 'json' };
+import artists from './artists.json' assert { type: 'json' };
 
-console.log(data);
+console.log(artists);
